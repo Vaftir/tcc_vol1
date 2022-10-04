3ad11338d7a8c45d5b0748100085ea1c1fb9a0d5
Vaftir <yagoassisfaria@gmail.com>
2022-10-04 12:38:59 -0300

    tela de correlação

--- user2.py
+++ user2.py
@@ -3,6 +3,8 @@ import PySimpleGUI as sg
 import cv2 as cv
 import io
 import os.path
+import numpy as np
+from matplotlib import pyplot as plt
 from PIL import Image
 
 
@@ -22,8 +24,10 @@ cropping = False
 Requerimentos:
     pip install Pillow
     pip install opencv-contrib-python
+    pip install matplotlib
 imageCut - > def para corte da imagem (a imagem recortada é salva como cropped.jpg
                                         na pasta photos do projeto)
+                                        OBS: o imageCut é um método pronto
             parametros:
                 x e y -> posiçoes do mouse
                 event -> evento para clique do mouse
@@ -50,7 +54,9 @@ def imageCut(event, x, y, flags, param):
             roi = oriImage[refPoint[0][1]:refPoint[1][1], refPoint[0][0]:refPoint[1][0]]
             cv.imshow("corte", roi)
             cv.imwrite(f'photos/cropped.jpg', roi)
-
+'''
+Primeira página -> menu
+'''
 def menu_window():
     sg.theme("DarkTeal2")
     menu_layout = [
@@ -68,9 +74,12 @@ def menu_window():
         elif event == 'Carregar arquivo':
             menu_window.close()
             view_img_window()
+        elif event == 'Correlação':
+            menu_window.close()
+            correlacao_window()
     menu_window.close()
 '''
-Primeira página -> menu
+Segunda página
 '''
 def view_img_window(): 
     sg.theme("DarkTeal2")
@@ -91,7 +100,7 @@ def view_img_window():
     ]
     img_view_window = sg.Window("Visualizador de imagens", layout)
     '''
-    Loop para rodar a aplicação
+    Loop para rodar a aplicação da segunda página
     '''
     while True: 
         event, values = img_view_window.read()
@@ -126,6 +135,58 @@ def view_img_window():
             menu_window()
             
     img_view_window.close()
+
+
+'''
+Terceira página
+'''
+def correlacao_window():
+    sg.theme("DarkTeal2")
+    layout = [
+        [sg.Image(key="-IMAGE-")],
+        [
+            sg.Text("Imagem A"),
+            sg.Input(size=(25,1), key= "-FILE-"),
+            sg.FileBrowse(file_types=file_types),
+            sg.Button("Submit"),
+        ],
+        [sg.Image(key="-IMAGE2-")],
+        [
+            sg.Text("Imagem B"),
+            sg.Input(size=(25,1), key= "-FILE2-"),
+            sg.FileBrowse(file_types=file_types,initial_folder='photos'),
+            sg.Button("Submit Crop"),
+        ],
+        [sg.Button("Menu")]
+    ]
+    correlacao_window = sg.Window("Correlação", layout)
+    '''
+    Loop da aplicação da terceira página
+    '''
+    while True: 
+       event, values = correlacao_window.read()
+       if event == sg.WIN_CLOSED or event=="Exit": #Evento para fechar a aplicação
+            break
+       elif (event == "Submit") and (values["-FILE-"]): #Botão submit que mostra o template da imagem
+            filename = values["-FILE-"]
+            if os.path.exists(filename):
+                image = Image.open(values["-FILE-"])
+                image.thumbnail((500,500))
+                bio = io.BytesIO()
+                aux = image.save(bio,format="PNG")
+                image.save(bio,format="PNG")
+                correlacao_window["-IMAGE-"].update(data=bio.getvalue())
+       elif (event == "Submit Crop") and (values["-FILE2-"]): #Botão submit que mostra o template da imagem
+            filename = values["-FILE2-"]
+            if os.path.exists(filename):
+                image = Image.open(values["-FILE2-"])
+                image.thumbnail((500,500))
+                bio = io.BytesIO()
+                image.save(bio,format="PNG")
+                correlacao_window["-IMAGE2-"].update(data=bio.getvalue()) 
+       elif event == "Menu": #Volta pro menu
+            correlacao_window.close()
+            menu_window()
 
 def main():
     menu_window()
